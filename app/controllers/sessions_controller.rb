8f3bf8d29b79f79e1efef28458eec88e91c208ba
RachDerra <abdoulrachidederra@gmail.com>
2022-04-09 13:57:59 +0000

    CorrectView

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to user_path(user.id)
+      redirect_to posts_path
     else
       flash.now[:danger] = 'Je nai pas réussi à me connecter'
       render :new
