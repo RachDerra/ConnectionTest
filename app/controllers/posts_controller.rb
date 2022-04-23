40edc74bae0198235fca8c8f6904649bfe783a7d
RachDerra <abdoulrachidederra@gmail.com>
2022-04-23 16:13:46 +0000

    UserImageValid

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    # @post = Post.new(post_params)
-    # @post.user_id = current_user.id
     @post = current_user.posts.build(post_params)
     if params[:back]
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  
 end
